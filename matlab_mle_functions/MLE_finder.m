dbc7f08614053793f2c85e844b0cdbebad03b416
plavskin <eugene.plavskin@gmail.com>
2018-12-14 15:29:07 -0500

    Ensured MLE_finder ran if 0 fitted parameters were provided by just calculating the likelihood given the fixed parameter values

--- matlab_mle_functions/MLE_finder.m
+++ matlab_mle_functions/MLE_finder.m
@@ -41,8 +41,11 @@ function MLE_finder(key_list, value_list)
     global_param_bool_list = parameter_identifier(parameter_list, global_mle_parameters);
     global_param_number = sum(global_param_bool_list);
     if global_param_number == 0
+        global_mle_parameter_names = parameter_list;
         global_param_bool_list = true(size(global_param_bool_list));
         global_param_number = sum(global_param_bool_list);
+    else
+        global_mle_parameter_names = intersect(parameter_list, global_mle_parameters);
     end
     % rescale parameters and convert to logspace as needed
     combined_min_array = value_rescaler(combined_min_array_unscaled,combined_logspace_array,combined_scaling_array);
@@ -102,74 +105,85 @@ function MLE_finder(key_list, value_list)
         pre_MLE_function = str2func(pre_MLE_function_name);
         pre_MLE_output_dict = pre_MLE_function(input_value_dict);
     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Set up global search structure
-
-    gs = GlobalSearch;
-    gs.TolFun = tolfun_val;
-    gs.TolX = tolx_val;
-    gs.NumStageOnePoints = 2^global_param_number; % default = 200; sufficient is 50
-    gs.NumTrialPoints = 3^global_param_number; % default = 1000; sufficient is 200
-    gs.StartPointsToRun='bounds';
-    gs.Display='final';
-
-    ms = MultiStart(gs);
-        % assign same Tol, StartPointsToRun, and Display properties to ms as to gs
-    if parallel_processors > 1
-        ms.UseParallel = 1;
-    else
-        ms.UseParallel = 0;
-    end
-
-    fmincon_opts = optimoptions('fmincon','TolX',tolx_val,'TolFun',tolfun_val,...
-        'Algorithm','interior-point','MaxIter',5000,'MaxFunEvals',12000,...
-        'SpecifyObjectiveGradient',gradient_specification,'CheckGradients',...
-        false,'Display','off');
-
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % if any parameters are fitted, run MLE; otherwise, just run
+        % LL_calculator directly
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     global_start_time = tic;
+    if sum(~global_fixed_parameter_indices) > 0
+        % Set up global search structure
 
-    min_problem_fixed_params = createOptimProblem('fmincon','objective',...
-        @(v) LL_calculator(v,...
-            global_fixed_parameter_indices,global_fixed_parameter_values,...
-            global_logspace_array,global_scaling_array, max_neg_LL_val, input_value_dict, pre_MLE_output_dict),...
-        'x0',global_start_vals_fitted,'lb',global_lower_bounds_fitted,'ub',global_upper_bounds_fitted,...
-        'options',fmincon_opts);
-    
-    if ms_positions == 0
-        % use globalsearch algorithm
-        [vout_current,fval_current,exitflag_current,output_current,solutions_current]=...
-            run(gs,min_problem_fixed_params);
-    elseif ms_positions == 1
-        % only use single startpoint, at global_start_values
-        [vout_current,fval_current,exitflag_current]=...
-            fmincon(min_problem_fixed_params);
-    else
+        gs = GlobalSearch;
+        gs.TolFun = tolfun_val;
+        gs.TolX = tolx_val;
+        gs.NumStageOnePoints = 2^global_param_number; % default = 200; sufficient is 50
+        gs.NumTrialPoints = 3^global_param_number; % default = 1000; sufficient is 200
+        gs.StartPointsToRun='bounds';
+        gs.Display='final';
 
+        ms = MultiStart(gs);
+            % assign same Tol, StartPointsToRun, and Display properties to ms as to gs
         if parallel_processors > 1
-            parpool('local',parallel_processors);
+            ms.UseParallel = 1;
+        else
+            ms.UseParallel = 0;
         end
 
-        %ms_positions_across_dimensions = ms_positions^global_param_number+1;
-        indices_to_multistart_fitted = indices_to_multistart(~global_fixed_parameter_indices);
+        fmincon_opts = optimoptions('fmincon','TolX',tolx_val,'TolFun',tolfun_val,...
+            'Algorithm','interior-point','MaxIter',5000,'MaxFunEvals',12000,...
+            'SpecifyObjectiveGradient',gradient_specification,'CheckGradients',...
+            false,'Display','off');
 
-        startpoints = MS_Startposition_Generator_v2(indices_to_multistart_fitted,ms_positions,...
-            global_start_vals_fitted,global_lower_bounds_fitted,global_upper_bounds_fitted);
-        [vout_current,fval_current,exitflag_current,output_current,solutions_current]=...
-            run(ms,min_problem_fixed_params,startpoints);
+        min_problem_fixed_params = createOptimProblem('fmincon','objective',...
+            @(v) LL_calculator(v,...
+                global_fixed_parameter_indices,global_fixed_parameter_values,...
+                global_logspace_array,global_scaling_array, max_neg_LL_val, input_value_dict, pre_MLE_output_dict),...
+            'x0',global_start_vals_fitted,'lb',global_lower_bounds_fitted,'ub',global_upper_bounds_fitted,...
+            'options',fmincon_opts);
+
+        if ms_positions == 0
+            % use globalsearch algorithm
+            [vout_current,fval_current,exitflag_current,output_current,solutions_current]=...
+                run(gs,min_problem_fixed_params);
+        elseif ms_positions == 1
+            % only use single startpoint, at global_start_values
+            [vout_current,fval_current,exitflag_current]=...
+                fmincon(min_problem_fixed_params);
+        else
+
+            if parallel_processors > 1
+                parpool('local',parallel_processors);
+            end
+
+            %ms_positions_across_dimensions = ms_positions^global_param_number+1;
+            indices_to_multistart_fitted = indices_to_multistart(~global_fixed_parameter_indices);
+
+            ms_starting_point_mat = NaN;
+            startpoints = MS_Startposition_Generator_v2(indices_to_multistart_fitted,ms_positions,...
+                global_start_vals_fitted,global_lower_bounds_fitted,global_upper_bounds_fitted,...
+                ms_starting_point_mat);
+            [vout_current,fval_current,exitflag_current,output_current,solutions_current]=...
+                run(ms,min_problem_fixed_params,startpoints);
+        end
+
+        vout_current_corrected = zeros(size(global_fixed_parameter_indices));
+        vout_current_corrected(global_fixed_parameter_indices) = global_fixed_parameter_values(~isnan(global_fixed_parameter_values));
+        vout_current_corrected(~global_fixed_parameter_indices) = vout_current;
+        vout_current_corrected = reverse_value_scaler(vout_current_corrected,global_logspace_array,global_scaling_array);
+    else
+        
+        [fval_current] = LL_calculator([],...
+                global_fixed_parameter_indices,...
+                global_fixed_parameter_values, global_logspace_array,...
+                global_scaling_array, max_neg_LL_val, input_value_dict,...
+                pre_MLE_output_dict);
+        vout_current_corrected = reverse_value_scaler(global_fixed_parameter_values,...
+            global_logspace_array,global_scaling_array);
     end
-    
-    vout_current_corrected = zeros(size(global_fixed_parameter_indices));
-    vout_current_corrected(global_fixed_parameter_indices) = global_fixed_parameter_values(~isnan(global_fixed_parameter_values));
-    vout_current_corrected(~global_fixed_parameter_indices) = vout_current;
-    vout_current_corrected = reverse_value_scaler(vout_current_corrected,global_logspace_array,global_scaling_array);
-
     runtime = toc(global_start_time);
     
-    % close current parallel pool
-    if parallel_processors > 1
-        delete(gcp('nocreate'));
-    end
-
     export_data = [-fval_current,runtime,vout_current_corrected];
 
     % if doing mutlistart or global search and one solution didn't converge, replace export_data with NA
@@ -185,7 +199,7 @@ function MLE_finder(key_list, value_list)
     %dlmwrite(output_file,export_data,'delimiter',',','precision',9);
 
     table_data = num2cell([-fval_current,runtime,vout_current_corrected,external_counter]);
-    table_headers = {'LL','runtime_in_secs',parameter_list{:},'point_num'};
+    table_headers = {'LL','runtime_in_secs',global_mle_parameter_names{:},'point_num'};
     T = table(table_data{:},'VariableNames',table_headers);
     writetable(T,output_file);
 
@@ -194,6 +208,12 @@ function MLE_finder(key_list, value_list)
         post_MLE_function(key_list, value_list, T);
     end
 
+    % close current parallel pool
+    if parallel_processors > 1
+        delete(gcp('nocreate'));
+    end
+
+    
     if pause_at_end & runtime < 120
         pausetime=120-runtime;
         pause(pausetime)
